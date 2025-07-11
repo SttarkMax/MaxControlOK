0268190689223c7d327e41f3145afdec9204a8bd
SttarkMax <f13moreira@gmail.com>
2025-07-11 01:49:02 -0300

    Fix broken icons for Dashboard, Clients, Quotes and Users

--- components/icons/BuildingOfficeIcon.tsx
+++ components/icons/BuildingOfficeIcon.tsx
@@ -1,9 +1,9 @@
-
 import React from 'react';
 
 const BuildingOfficeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M8.25 21V3m8.25 18V3M3 3h18M3 21h18m-9-18v18M4.5 12H6m12 0h1.5m-15 0H9m6 0h1.5m-5.25-3H9m5.25 0h1.5M4.5 9H6m12 0h1.5m-15 0H9m6 0h1.5m-5.25-3H9m5.25 0h1.5m-5.25-3H9m5.25 0h1.5" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 21h19.5m-18-18v18m2.25-18v18m13.5-18v18M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h18M12 3v18" />
   </svg>
 );
+
 export default BuildingOfficeIcon;
