dfb8097869fdf32b2967b9b804f6c7de2545f95e
SttarkMax <f13moreira@gmail.com>
2025-07-12 14:40:40 -0300

    Fix Input import in UsersPage.tsx

--- pages/UsersPage.tsx
+++ pages/UsersPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Button from '../components/common/Button';
-import { Input } from '../components/common/Input';
+import Input from '../components/common/Input';
 import { Select } from '../components/common/Select';
 import { PlusIcon } from '../components/icons/PlusIcon';
 import { PencilIcon } from '../components/icons/PencilIcon';
